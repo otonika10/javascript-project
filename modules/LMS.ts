3a0b1c50b897aeae1835c13ee4f65179717bf508
otonika10 <77378386+otonika10@users.noreply.github.com>
2021-09-06 15:57:25 +0400

    LMS

---
+++ modules/LMS.ts
@@ -0,0 +1,114 @@
+interface Sub{
+    id?:string
+
+}
+interface sub2{
+    title:string
+    lessons:number
+}
+interface sub3 extends Sub,sub2{
+   readonly description?:any
+}
+
+interface Sub_Check{
+    id:string
+    title:string
+
+}
+interface Sub_Check2{
+    lessons:number
+    description?:string
+}
+//polymorphism
+class subs {
+    name: string;
+    lesson:number;
+    constructor(theName: string,thelesson:number) { this.name = theName
+    this.lesson = thelesson; }
+    p() {
+        //console.log(this.name+ " lesson " +this.lesson);
+    }
+}
+class sub extends subs {
+    constructor(name: string,lesson:number) { super(name,lesson); }
+    p() {
+        super.p();
+    }
+}
+let subj1: subs[] = [new sub('history',1)];
+subj1.forEach(subs => {
+    subs.p();
+})
+//abstract
+abstract class Department {
+    constructor(public title: string) {}
+    static Id:number = 1;
+    getId(): string {
+        return this.title +" "+ "Sub# " + Department.Id++;
+    }
+
+    abstract printId(): void;
+}
+class Item extends Department{
+    constructor(public name: string) {
+        super(name);
+    }
+
+    printId() {
+        const id: string = super.getId();
+        return id
+    }
+}
+const item: Item = new Item('');
+item.printId();
+
+
+export class Subject{
+    static Id:number = 1;
+    id:string;
+    title:string;
+    lessons:number;
+    description:any;
+    constructor(name:sub3){
+        this.id ="Sub# " + Subject.Id++;
+        this.title = name.title;
+        this.lessons = name.lessons;
+        if(typeof(name.description)!='undefined'){
+            this.description = name.description
+        }
+        if(typeof(name.lessons)!="number"){
+            throw new Error("Error: incorrect type")
+        }
+        if(typeof(name.title)!="string"){
+            throw new Error("Error: incorrect type")
+        }
+    }
+}
+export class subj extends Subject{
+    constructor(name:sub3){super(name)}
+    
+}
+export default class LMS {
+    sub:any = [];
+    add(name:Sub_Check & Sub_Check2){
+        this.sub.push(name)
+        //console.log(name)
+    }
+    remove(name:Sub_Check & Sub_Check2) {
+        this.sub.splice(this.sub.indexOf(name), 1);
+    }
+    verify(name:Sub_Check & Sub_Check2):Boolean {
+        return this.sub.indexOf(name) != -1;
+    }
+    readAll():Sub_Check & Sub_Check2 {
+        return this.sub;
+    }
+}
+export const history1:Sub_Check & Sub_Check2 = new Subject({
+    title: 'History',
+    lessons: 24 
+});
+export const math:Sub_Check & Sub_Check2 = new Subject({
+    title: 'Math',
+    lessons: 24
+}); 
