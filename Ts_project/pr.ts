7aef7cdbd84349e74331826988335fb21b3878ad
otonika10 <77378386+otonika10@users.noreply.github.com>
2021-09-06 11:52:23 +0400

    feat(pupil)

--- Ts_project/pr.ts
+++ Ts_project/pr.ts
@@ -285,3 +285,145 @@ const newTeacherId = teachers.update(teacherId, updatedProfile);
 //console.log(teachers.read(teacherId))
 teachers.remove(teacherId)
 //console.log(teachers.read(teacherId))
+//pupil
+class  Pupils{
+
+    private pupils = new Map();
+    
+     private validate(pupil:any) {
+        for(let i in pupil.dateOfBirth){
+            let Reg = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+            if (!Reg.test(pupil.dateOfBirth)) {
+                throw new Error("Error: Invalid date");
+            }
+        }
+        for(let i in pupil.phones){
+            let Reg = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+            if(!Reg.test(pupil.phones[i].phone)) {
+                throw new Error("Error: number is not valid");
+            }
+        } 
+    } 
+    obj:any
+    add(name:Pup_Check){
+        
+        let obj = {id:""}
+        this.obj=name
+        this.validate(name)
+
+        if (name.sex != 'male' && name.sex != 'female') {
+            throw new Error("Error: not defined 'male' or 'female'");
+        }
+        if (typeof (name.sex) == "undefined") {
+            throw new Error("Error: not defined");
+        }
+        obj.id = "PUP#" + Math.floor(Math.random()*900+100);
+        this.pupils.set(obj.id,{obj, name});
+        //console.log(obj)
+        return obj
+        
+    }
+
+    read <T>(pupilId:T):T {//Generic
+        return this.pupils.get(pupilId);
+    }
+
+    update(pupilId:string, updatedProfile2:any) {
+        for (const i in updatedProfile2) {
+            this.pupils.get(pupilId)[i] = updatedProfile2[i];
+        }
+    }
+
+    remove(pupilId:string) {
+        this.pupils.delete(pupilId);
+    }
+    show(pupilId:string){
+        return this.pupils.get(pupilId).name
+    }
+}
+
+interface Pup_Check{
+    name:{first:string, last:string}
+    image:string
+    dateOfBirth:string
+    phones:{phone:string;primary:boolean}[];
+    sex:string
+}
+const pup1:Pup_Check = {
+    name: {
+        first: "mir",
+        last: "lom"
+    },
+    image: "photo",
+    dateOfBirth: "07.04.2001",
+    phones: [
+        {
+            phone: "595-02-75-07",
+            primary: true
+        }
+    ],
+    sex: "male",
+};
+
+const pup2:Pup_Check = {
+    name: {
+        first: "Vano",
+        last: "Smash"
+    },
+    phones: [
+        {
+            phone: "599-98-89-89",
+            primary: true
+        }
+    ],
+    image: "vanessa_image",
+    dateOfBirth: "02.10.1994",
+    sex: "female"
+}
+const pup3:Pup_Check = {
+    name: {
+        first: "damn",
+        last: "nmad"
+    },
+    phones: [
+        {
+            phone: "599-98-89-81",
+            primary: true
+        }
+    ],
+    image: "vanessa_image",
+    dateOfBirth: "02.07.1974",
+    sex: "female"
+}
+const pupils = new Pupils();
+// Create a new pupil
+
+//console.log(pupils.add(pup1))
+// will return Pupils data including pupil's id
+const pupil:{id:string} = pupils.add(pup1);
+const pupil2:{id:string} = pupils.add(pup2);
+const pupil3:{id:string} = pupils.add(pup3);
+const updatedProfile2:upd = {
+    name: {
+        first: "miranda",
+        last: "lomouri"
+    },
+    dateOfBirth: "14.04.1973",
+    subjects: [
+        {
+            subject: "Math"
+        }
+    ]
+}
+// will update Pupil. This method should use the same validation as a constructor method
+pupils.update(pupil.id, updatedProfile2)
+var show0 = pupils.show(pupil.id)
+var show = pupils.show(pupil.id)
+var show2:Pup_Check = pupils.show(pupil2.id)
+var show3:Pup_Check = pupils.show(pupil3.id)
+//console.log(show.name.first)
+var  fullpupil = pupils.read(pupil2.id);
+
+// will remove pupil
+pupils.remove(pupil.id);
+//console.log(pupils.read(pupil.id));
