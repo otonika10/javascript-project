8f0667286f9bce83348e61a5904066ffac396385
otonika10 <77378386+otonika10@users.noreply.github.com>
2021-09-06 11:53:25 +0400

    feat(groups)

--- Ts_project/pr.ts
+++ Ts_project/pr.ts
@@ -427,3 +427,70 @@ var  fullpupil = pupils.read(pupil2.id);
 // will remove pupil
 pupils.remove(pupil.id);
 //console.log(pupils.read(pupil.id));
+
+//groups
+
+class Groups{
+     groups = new Map();
+     arr:any = []
+    add(room:number):string {
+        
+        let id:string = "JEF" + Math.floor(Math.random()*9+1)+"H"+ Math.floor(Math.random()*90+10)+"H";
+        this.groups.set(id, { id, room,});
+        return id;
+    }
+    
+    addPupil(groupId:string, pupilId:{id:string}) {
+            
+            this.arr.push(pupilId)
+            this.groups.set(room,this.arr)
+            
+            /* this.groups.pupils = [];
+            this.groups.pupils.push(pupilId); */
+            //console.log(pupilId.id)
+    }
+
+    removePupil(groupId:string, pupilId:string) {
+        //console.log(this.#groups)
+/*         for(let i in this.groups.pupils){
+            if(this.groups.pupils[i].id==pupilId){
+                this.groups.pupils.splice(i,1)
+                //console.log(this.#groups)
+            }
+        } */
+        for(let i in this.groups.get(room)){
+            if(this.groups.get(room)[i].id==pupilId){
+                this.groups.get(room).splice(i,1)
+                //console.log(this.groups.get(room))
+            }
+        } 
+    }
+    update(groupId:string, obj:{room:number}) {
+        for (const i in obj) {
+            this.groups.get(groupId)[i] = obj;
+            //console.log(this.#groups.get(groupId)[i])
+        }
+    }
+
+    read <T>(groupId:T):T {//Generic
+        //console.log(this.#groups.get(groupId))
+        return this.groups.get(groupId);
+    }
+
+    readAll() {
+        //console.log(Array.from(this.#groups))
+        return Array.from(this.groups);
+    }
+}
+
+const room:number = 236;
+const groups = new Groups();
+const groupId:string = groups.add(room);
+//console.log(groups.add(room))
+groups.addPupil(groupId, pupil);
+groups.removePupil(groupId, pupil.id);
+groups.update(groupId, {
+    room: 237
+  });
+groups.read(groupId);
+groups.readAll()
